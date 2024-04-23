86933b3cc49e6b58351b2767e9c553937a88e7da
maheshsSMBXL <maheshs@smbxl.com>
2024-04-23 10:46:51 +0530

    first commit

---
+++ AadharVerification/Controllers/ConvertImageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace AadharVerification.Controllers
+{
+    public class ConvertImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("ImageToBase64")]
+        public void ImageToBase64(IFormFile file) 
+        {
+
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    byte[] fileByte = ms.ToArray();
+                    string Base64 = Convert.ToBase64String(fileByte);
+                }
+            }                      
+        }
+    }
+}
